d20417da28932cb6812f4df3fced636bdf7a8603
Kyler Eastridge <felix.wolfz@gmail.com>
2025-06-18 08:09:11 -0400

    Track circuitCode

--- pymetaverse/viewer/agent.py
+++ pymetaverse/viewer/agent.py
@@ -9,6 +9,7 @@ class Agent(EventTarget):
         self.simulator = None
         self.simulators = []
         self.messageTemplate = messages.getDefaultTemplate()
+        self.circuitCode = None
     
     async def addSimulator(self, host, circuit, caps = None, parent = False):
         sim = Simulator(self)
@@ -35,16 +36,21 @@ class Agent(EventTarget):
     @classmethod
     async def fromLogin(cls, login):
         self = cls()
+        
+        if login["login"] == False:
+            raise ValueError("Invalid login handle")
+        
         self.agentId = login["agent_id"]
         self.sessionId = login["session_id"]
         self.secureSessionId = login["secure_session_id"]
+        self.circuitCode = login["circuit_code"]
         
-        await self.addSimulator((login["sim_ip"], login["sim_port"]), login["circuit_code"], login["seed_capability"], True)
+        await self.addSimulator((login["sim_ip"], login["sim_port"]), self.circuitCode, login["seed_capability"], True)
         
         msg = self.messageTemplate.getMessage("CompleteAgentMovement")
         msg.AgentData.AgentID = self.agentId
         msg.AgentData.SessionID = self.sessionId
-        msg.AgentData.CircuitCode = login["circuit_code"]
+        msg.AgentData.CircuitCode = self.circuitCode
         self.send(msg, True)
         
         return self
